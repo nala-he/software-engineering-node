16975b3a262ab92971aec5a45975ec572d446065
nala-he <hytdaisy@gmail.com>
2022-11-16 22:53:46 -0500

    created DislikeSchema

---
+++ mongoose/DislikeSchema.ts
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+/**
+ * @file Implements mongoose schema in the likes collection
+ */
+const dislikeSchema = new mongoose.Schema({
+    dislikedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'},
+    dislikedTuit: {type: mongoose.Schema.Types.ObjectId, ref: 'TuitModel'}
+}, {collection: 'dislikes'});
+export default dislikeSchema;
