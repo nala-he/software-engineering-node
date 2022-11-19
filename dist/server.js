dec14805c1ce5349ffc3fe61daf7800793b0fce2
nala-he <hytdaisy@gmail.com>
2022-11-19 10:31:00 -0500

    removed DB credentials, set up DB credentials info in env

--- dist/server.js
+++ dist/server.js
@@ -51,10 +51,8 @@ if (process.env.ENV === 'PRODUCTION') {
 app.use(session(sess));
 // build the connection string
 const PROTOCOL = "mongodb+srv";
-// const DB_USERNAME = process.env.DB_USERNAME;
-// const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_USERNAME = "fse-nala";
-const DB_PASSWORD = "H6GVmTUTH5KEZNS0";
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 const HOST = "cluster0.kndb1tp.mongodb.net";
 const DB_NAME = "fse";
 const DB_QUERY = "retryWrites=true&w=majority";
--- server.ts
+++ server.ts
@@ -57,10 +57,9 @@ app.use(session(sess));
 
 // build the connection string
 const PROTOCOL = "mongodb+srv";
-// const DB_USERNAME = process.env.DB_USERNAME;
-// const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_USERNAME = "fse-nala";
-const DB_PASSWORD = "H6GVmTUTH5KEZNS0";
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
 const HOST = "cluster0.kndb1tp.mongodb.net";
 const DB_NAME = "fse";
 const DB_QUERY = "retryWrites=true&w=majority";
