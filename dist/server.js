ba04327005fd71774347ae91a050a81e7498e043
nala-he <hytdaisy@gmail.com>
2022-11-20 17:33:08 -0500

    changed corsConfig credentials back to true

--- dist/server.js
+++ dist/server.js
@@ -34,7 +34,7 @@ const corsConfig = {
         'https://a4-sparkly-macaron-8217a9-fse.netlify.app/',
         'http://localhost:3000'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    credentials: false,
+    credentials: true,
     optionSuccessStatus: 200,
 };
 app.use(cors(corsConfig));
